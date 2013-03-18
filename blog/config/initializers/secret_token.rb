b2150317b0ee99fa7eac39f0179a06fb6b295073
Amaresh Pattanaik <apattanaik@paypal.com>
2013-03-18 16:24:23 -0500

    new sample project.

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '9e6f68502f6dc8e839fda379d0fa9bfc2ac1fc62b650fce60ae755f2ec1ee42bc076066c544f311604316d63d9fa5ac0c051b485416798ea9015163e58913d04'
